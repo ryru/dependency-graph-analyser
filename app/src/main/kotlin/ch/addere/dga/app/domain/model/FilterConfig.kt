c819813dd3d435d8c96234dd04d229cc355c2352
Pascal Knecht <pascal@addere.ch>
2023-11-10 17:31:33 +0100

    Simplify filter config initialisation

--- app/src/main/kotlin/ch/addere/dga/app/domain/model/FilterConfig.kt
+++ app/src/main/kotlin/ch/addere/dga/app/domain/model/FilterConfig.kt
@@ -4,25 +4,8 @@ import ch.addere.dga.graph.domain.model.Configuration
 import ch.addere.dga.graph.domain.model.Module
 
 data class FilterConfig(
-    private val modules: List<Module>?,
-    private val originModules: List<Module>?,
-    private val destinationModules: List<Module>?,
-    private val configurations: List<Configuration>?
-) {
-
-    fun modules(): List<Module> {
-        return modules ?: emptyList()
-    }
-
-    fun originModules(): List<Module> {
-        return originModules ?: emptyList()
-    }
-
-    fun destinationModules(): List<Module> {
-        return destinationModules ?: emptyList()
-    }
-
-    fun configurations(): List<Configuration> {
-        return configurations ?: emptyList()
-    }
-}
+    val modules: List<Module>,
+    val originModules: List<Module>,
+    val destinationModules: List<Module>,
+    val configurations: List<Configuration>
+)
