3c73fdb6f28bbb9daa2a6ce2854344d8634df0a3
Pascal Knecht <pascal@addere.ch>
2024-09-06 18:46:55 +0200

    Separate GradleConnectorService into interface and infrastructure implementation

--- app/src/main/kotlin/ch/addere/dga/app/infrastructure/factory/ImporterModule.kt
+++ app/src/main/kotlin/ch/addere/dga/app/infrastructure/factory/ImporterModule.kt
@@ -4,12 +4,13 @@ import ch.addere.dga.importer.application.service.GradleProjectLoader
 import ch.addere.dga.importer.application.service.GradleProjectLoaderImpl
 import ch.addere.dga.importer.domain.service.GradleConnectorService
 import ch.addere.dga.importer.domain.service.ModelImporterService
+import ch.addere.dga.importer.infrastructure.GradleConnectorServiceImpl
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val importerModule = module {
-    singleOf(::GradleConnectorService)
-    singleOf(::ModelImporterService)
+    singleOf(::GradleConnectorServiceImpl) { bind<GradleConnectorService>() }
     singleOf(::GradleProjectLoaderImpl) { bind<GradleProjectLoader>() }
+    singleOf(::ModelImporterService)
 }
---
+++ importer/src/main/kotlin/ch/addere/dga/importer/infrastructure/GradleConnectorServiceImpl.kt
@@ -0,0 +1,51 @@
+package ch.addere.dga.importer.infrastructure
+
+import ch.addere.dga.dependencymodel.DependencyModel
+import ch.addere.dga.importer.domain.service.GradleConnectorService
+import org.gradle.tooling.GradleConnector
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.io.path.absolutePathString
+import kotlin.io.path.createTempDirectory
+
+class GradleConnectorServiceImpl : GradleConnectorService {
+
+    private val scriptFileName = "init.gradle.kts"
+    private val scriptParentDir = "dga"
+
+    override fun connectToGradle(gradleProjectFolder: File): DependencyModel {
+
+        val pathToInitScript = createInitScript()
+
+        val connector = GradleConnector.newConnector()
+        connector.forProjectDirectory(gradleProjectFolder)
+
+        val dependencyModel: DependencyModel = connector.connect().use { con ->
+            val customModelBuilder = con.model(DependencyModel::class.java)
+            customModelBuilder.withArguments("--init-script", pathToInitScript.absolutePathString())
+            customModelBuilder.get()
+        }
+
+        deleteInitScript(pathToInitScript)
+        return dependencyModel
+    }
+
+    private fun createInitScript(): Path {
+        val osTempDir = createTempDirectory().parent
+        val scriptDir = Files.createDirectories(Path("$osTempDir/$scriptParentDir/"))
+        val initScript = File("$scriptDir/$scriptFileName")
+        initScript.writeText(javaClass.getResource("/$scriptFileName")!!.readText())
+        return initScript.toPath()
+    }
+
+    private fun deleteInitScript(pathToInitScript: Path) {
+        if (pathToInitScript.endsWith(scriptFileName)) {
+            Files.delete(pathToInitScript)
+        }
+        if (pathToInitScript.parent.endsWith(scriptParentDir)) {
+            Files.delete(pathToInitScript.parent)
+        }
+    }
+}
