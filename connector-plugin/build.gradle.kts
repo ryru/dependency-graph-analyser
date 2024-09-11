14d288bf70811a528910a661297e497eafbb5063
Pascal Knecht <pascal@addere.ch>
2024-09-11 21:01:34 +0200

    Include dependency model into connector plugin by applying shadow plugin

--- connector-plugin/build.gradle.kts
+++ connector-plugin/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     id("ch.addere.dga.kotlin-library-conventions")
     `java-gradle-plugin`
-    `maven-publish`
-    id("com.gradle.plugin-publish") version "1.2.1"
+    id("com.gradle.plugin-publish") version "1.3.0"
 }
 
 description = "Gradle plugin that is injected into to be analysed Gradle projects"
@@ -29,23 +28,4 @@ gradlePlugin {
             implementationClass = "ch.addere.dga.connectorplugin.ConnectorPlugin"
         }
     }
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "pluginRepo"
-            url = uri(layout.projectDirectory.dir("../build/pluginRepo"))
-        }
-
-        publications {
-            create<MavenPublication>("maven") {
-                from(components["kotlin"])
-            }
-        }
-    }
-}
-
-tasks.withType<PublishToMavenLocal> {
-    dependsOn(tasks.getByPath(":connector-model:publishToMavenLocal"))
 }
